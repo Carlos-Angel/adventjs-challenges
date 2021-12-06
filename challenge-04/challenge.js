467a4c902fad32a0e2ec985f3452508a91130275
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-05 18:30:01 -0600

    fix: remover función repetida

--- challenge-04/challenge.js
+++ challenge-04/challenge.js
@@ -33,27 +33,3 @@ function createXmasTree(height) {
 }
 
 console.log(createXmasTree(1));
-
-export default function createXmasTree(height) {
-  let tree = '';
-  let nivel = 1;
-
-  for (let index = 1; index < height; index++) {
-    const repeatUnderscore =
-      index === 1 ? (height * 2) / 2 - 1 : (height * 2) / 2 - nivel / 2;
-
-    const underscores = '_'.repeat(repeatUnderscore);
-    tree += `${underscores}${'*'.repeat(nivel)}${underscores}\n`;
-
-    nivel += 2;
-  }
-
-  tree += `${'*'.repeat(nivel)}\n`;
-
-  const underscores = '_'.repeat((height * 2) / 2 - 1);
-  tree += `${underscores}${'#'}${underscores}\n`;
-  tree += `${underscores}${'#'}${underscores}`;
-  return tree;
-}
-
-console.log(createXmasTree(1));
