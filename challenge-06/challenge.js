b1b9e5d1a29824555f407c4ed73b89c2d2cb37e7
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-07 17:25:36 -0600

    feat: reto 06 completado

--- challenge-06/challenge.js
+++ challenge-06/challenge.js
@@ -1,4 +1,21 @@
-export default function sumPairs(numbers, result) {
-  // ¡Y no olvides compartir tu solución en redes!
-  return null
+function sumPairs(numbers, result) {
+  const numbersTwo = [...numbers];
+  let a, b;
+  numbers.forEach((number, index) => {
+    numbersTwo.forEach((item, index2) => {
+      if (!a && !b) {
+        if (number + item === result && index !== index2) {
+          a = number;
+          b = item;
+        }
+      }
+    });
+  });
+  return a || b ? [a, b] : null;
 }
+
+console.log(sumPairs([3, 5, 7, 2], 10)); // [3, 7]
+console.log(sumPairs([-3, -2, 7, -5], 10)); // null
+console.log(sumPairs([2, 2, 3, 1], 4)); // [2, 2]
+console.log(sumPairs([6, 7, 1, 2], 8)); // [6, 2]
+console.log(sumPairs([0, 2, 2, 3, -1, 1, 5], 6)); // [1, 5]
