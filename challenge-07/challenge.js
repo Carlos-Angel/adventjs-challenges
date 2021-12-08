81f4fa32131b63dabb88302a6ed67075355f3a9e
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-07 18:55:28 -0600

    feat: reto 07 completado

--- challenge-07/challenge.js
+++ challenge-07/challenge.js
@@ -1,4 +1,35 @@
-export default function contains(store, product) {
-  // ¡Y no olvides compartir tu solución en redes!
-  return false
+function contains(store, product) {
+  let existProduct = false;
+
+  Object.keys(store).forEach((item) => {
+    if (typeof store[item] === 'string') {
+      if (store[item] === product) {
+        existProduct = true;
+      }
+    } else {
+      existProduct = contains(store[item], product);
+    }
+
+  });
+
+  return existProduct;
 }
+
+const almacen = {
+  estanteria1: {
+    cajon1: {
+      producto1: 'coca-cola',
+      producto2: 'fanta',
+      producto3: 'sprite',
+    },
+  },
+  estanteria2: {
+    cajon1: 'vacio',
+    cajon2: {
+      producto1: 'pantalones',
+      producto2: 'camiseta', // <- ¡Está aquí!
+    },
+  },
+};
+
+console.log(contains(almacen, 'camiseta'));
